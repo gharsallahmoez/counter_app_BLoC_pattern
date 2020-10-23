1137b9cc5611c3d765df45c1821a6a512c28e1c4
gharsallahmoez <gharsallahmoez@gmail.com>
2020-10-23 15:07:01 +0100

    integrate bloc in view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,13 @@
+import 'package:counter_app/bloc/counterBloc.dart';
+import 'package:counter_app/bloc/counterEvents.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // counter bloc accessor
+    CounterBloc cb = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -15,12 +20,18 @@ class Home extends StatelessWidget {
           children: [
             IconButton(
               icon: Icon(Icons.remove),
-              onPressed: () {},
+              onPressed: () {
+                cb.add(CounterEvents.remove);
+              },
             ),
-            Text('0'),
+            BlocBuilder<CounterBloc, int>(
+              builder: (context, state) => Text('$state'),
+            ),
             IconButton(
               icon: Icon(Icons.add),
-              onPressed: () {},
+              onPressed: () {
+                cb.add(CounterEvents.add);
+              },
             )
           ],
         ),
